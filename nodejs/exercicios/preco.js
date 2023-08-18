1e7809dcc868df168291c0c39feae2d60ca55222
Vitor F. Bergami <115746569+vitorfb33@users.noreply.github.com>
2023-08-18 20:22:23 -0300

    Create preco.js

---
+++ nodejs/exercicios/preco.js
@@ -0,0 +1,31 @@
+/**
+ * desenvolver um APP para calcular o preço de venda de um produto em função do preço 
+ * de custo e margem de lucro(%)
+ * 
+ */
+
+//importação do pacote readline-sync
+const read = require('readline-sync')
+
+
+console.log("+-+-+-+-+-+-+-+-+")
+console.log("|c|a|l|c|u|l|a|r|")
+console.log("+-+-+-+-+-+-+-+-+")
+
+
+
+//variavel
+let custo, lucro, vendas 
+
+//entrada
+custo = Number(read.question("Digite o custo: ").replace(",","."))
+lucro = Number(read.question("Digite o lucro: ").replace(",","."))
+
+//Processamento
+
+venda = custo + ((lucro * custo) /100)
+
+//saida
+
+console.clear()
+console.log(`total de venda foi ${venda} R$` )
