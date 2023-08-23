6c16d33f98a048a75dd75925d2837633312f3335
Vitor F. Bergami <115746569+vitorfb33@users.noreply.github.com>
2023-08-23 01:14:52 -0300

    Create horas.js

---
+++ nodejs/exercicios/horas.js
@@ -0,0 +1,19 @@
+//variavel
+let remuneracao, custo, investimento, horas
+const reserva = 30
+
+//entrada
+remuneracao = Number(read.question("Digite a remuneração: ").replace(",","."))
+reserva 
+custo = Number(read.question("Digite o custo: ").replace(",","."))
+investimento = Number(read.question("Digite o investimento(%): ").replace(",","."))
+horas
+
+//Processamento
+
+horas = (remuneracao +(((remuneracao * reserva)/100) + (custo +(remuneracao * investimento)/ 100)))
+horas = horas / 160
+//saida
+
+console.clear()
+console.log(`valor da hora de serviço ${horas.toFixed(2)} R$`)
