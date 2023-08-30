67a36523b4b15effb6b41bbfa35a6d1bec457461
Vitor F. Bergami <115746569+vitorfb33@users.noreply.github.com>
2023-08-29 22:13:47 -0300

    Create  gasolina.js

---
+++ nodejs/exercicios/gasolina.js
@@ -0,0 +1,34 @@
+/**
+ * gasolina vs etanol
+ */
+
+//importação do pacote readline-sync
+const read = require('readline-sync')
+
+//importação do pacote colors
+const colors = require("colors")
+
+//variavel
+let gasolina, etanol, resultado
+
+
+console.clear
+console.log("======================")
+console.log("= gasolina x entanol =")
+console.log("======================")
+console.log("")
+
+//entrada
+etanol = Number(read.question("quanto de gasolina: ").replace(",","."))
+gasolina = Number(read.question("quanto de etanol: ").replace(",","."))
+
+//processamento
+
+resultado = gasolina/etanol
+
+//saida
+ if ( resultado <= 0.7    ){
+    console.log("Abastecer com alcool".green)
+} else {
+    console.log("Abastecer com gasolina".red)
+}
