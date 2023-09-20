0d19f2feaf3ea5cebd37ecdbfdc2ab7b465dd869
Vitor F. Bergami <115746569+vitorfb33@users.noreply.github.com>
2023-09-20 19:11:03 -0300

    Create for.js

---
+++ nodejs/fundamentos/for.js
@@ -0,0 +1,14 @@
+/**
+ * estudo do laço for
+ */
+
+console.clear()
+for(let i = 1; i < 11; i++){
+    console.log(i)
+}
+console.log("Fim do laço for ")
+console.log("____________________________")
+for(let j = 10; j  > 0; j--){
+    console.log(j)
+}
+console.log("ignição")
