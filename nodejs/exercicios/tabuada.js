b352cb9c166308e640a0e7c377c04da75227837a
Vitor F. Bergami <115746569+vitorfb33@users.noreply.github.com>
2023-09-20 19:08:15 -0300

    Create tabuada.js

---
+++ nodejs/exercicios/tabuada.js
@@ -0,0 +1,16 @@
+/**
+ * Exemplo do uso do laço for
+ */
+//importação do pacote readline-sync
+const read = require('readline-sync')
+
+let valor 
+
+
+console.clear()
+console.log('tabuada__________________________')
+valor = Number(read.question('digite um valor da tabuada: '))
+
+for (let i = 1; i < 11; i++){
+    console.log(`${valor} x ${i} = ${valor * i} `)
+}
