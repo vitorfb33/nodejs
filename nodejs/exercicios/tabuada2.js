9d863020f6daf9df992cdaee40d1a43c3f1ed732
Vitor F. Bergami <115746569+vitorfb33@users.noreply.github.com>
2023-09-20 19:09:49 -0300

    Create tabuada2.js

---
+++ nodejs/exercicios/tabuada2.js
@@ -0,0 +1,12 @@
+/**
+ * Estudo do laço for (uso de 2 laços)
+ */
+
+for (let i = 1; i < 11; i++){
+    console.log("")
+    console.log(`tabuada do ${i}`)
+    console.log("")
+    for (let j = 1; j < 11; j++){
+        console.log(`${i} x ${j} = ${i * j} `)
+    }
+}
