00a22341362370cba1b0219588811897ee1f7c82
Vitor F. Bergami <115746569+vitorfb33@users.noreply.github.com>
2023-10-05 12:26:09 -0300

    Update minecraft.js

--- nodejs/poo/minecraft.js
+++ nodejs/poo/minecraft.js
@@ -1,17 +1,17 @@
 /**
  * Poo - Fundamentos da programação Orientada a objetos
  * Exemplo do jogo minecraft
- */ 
+ */
 
 // classe modelo (Abstração)
-class Bloco{
+class Bloco {
     //atributos //constructor dentro da classe modelo cria os atributos
-    constructor(resistencia, textura){
+    constructor(resistencia, textura) {
         this.resistencia = resistencia
         this.textura = textura
     }
     //ações (métodos)
-    criarBloco(){
+    criarBloco() {
         console.log("-----------------------------------------")
         console.log("┌──┐")
         console.log("└──┘")
@@ -19,18 +19,18 @@ class Bloco{
         console.log(`Resistencia: ${this.resistencia}`)
     }
 
-    construir(){
+    construir() {
         console.log(`Bloco de ${this.textura} colocado `)
     }
 
-    minerar(){
+    minerar() {
         console.log("☐ ☐ ☐ Recursos obtidos!")
     }
 }
 // classe modelo com herança(extends)
-class Enxada extends Bloco{
+class Enxada extends Bloco {
     //atributos
-    constructor(resistencia, textura, conquista){
+    constructor(resistencia, textura, conquista) {
         super(resistencia, textura)//super serve para puxar a classe já feita //this. cria 
         this.conquista = conquista
     }
@@ -45,20 +45,68 @@ class Enxada extends Bloco{
     }
     arar() {
         console.log("._._._._. Terra arada!")
-        if (this,this.conquista === true){
+        if (this, this.conquista === true) {
             console.log("☀ conquista obtida!")
         }
     }
 
+
     //Polimorfismo
-    minerar(){
+    minerar() {
         console.log("♰ dano atribuido!")
     }
 }
 
+class Espada extends Bloco {
+    //atributos
+    constructor(resistencia, textura) {
+        super(resistencia, textura)
+    }
+
+    criarEspada() {
+        console.log("-----------------------------------------")
+        console.log("|")
+        console.log("=")
+        console.log("'")
+        console.log(`Espada de ${this.textura}`)
+        console.log(`Resistençia da Espada ${this.resistencia}`)
+    }
+
+    //Polimorfismo
+    minerar() {
+        console.log("♰ dano atribuido!")
+    }
+}
+
+class VaraDePesca extends Bloco{
+    //Atributos
+    constructor(resistencia, textura, pesca){
+        super(resistencia, textura)
+        this.pesca = pesca
+    }
+
+    criarvara() {
+        console.log("-----------------------------------------")
+        console.log(" /◝")
+        console.log("/   ◝")
+        console.log("      *")
+        console.log(`Vara de pesca de ${this.textura}`)
+        console.log(`Resistencia da Vara de Pesca ${this.resistencia}`)
+    }
+    pescar() {
+        console.log("~~~~~~~~~~~ Pesca concluida!")
+        if (this, this.pesca === true) {
+            console.log("☀ conquista obtida Pesca!")
+        }
+    }
+
+}
+
+
+
 //criação de objetos (Mundo)
 console.clear()
-                                                      
+
 console.log(" _____ _____ _____ _____ _____ _____ _____ _____ _____ ")
 console.log("|     |     |   | |   __|     | __  |  _  |   __|_   _|")
 console.log("| | | |-   -| | | |   __|   --|    -|     |   __| | |  ")
@@ -68,9 +116,9 @@ console.log("|_|_|_|_____|_|___|_____|_____|__|__|__|__|__|    |_|  ")
 //criando um bloco de terra // new cria um novo objeto
 //new (novo objeto)
 //bloco (classe modelo(resistencia, textura))
-const bloco1  = new Bloco(1, "terra")
+const bloco1 = new Bloco(1, "terra")
 bloco1.criarBloco()
- 
+
 //criando um bloco de madeira
 const bloco2 = new Bloco(2, "madeira")
 bloco2.criarBloco()
@@ -90,7 +138,7 @@ bloco4.criarBloco()
 
 //criando uma enxada de madeira
 
-const enxada1 = new  Enxada(2, "madeira", false)
+const enxada1 = new Enxada(2, "madeira", false)
 enxada1.criarEnxada()
 enxada1.arar()
 
@@ -100,7 +148,32 @@ const enxada2 = new Enxada(5, "ferro", true)
 enxada2.criarEnxada()
 enxada2.arar()
 
-
+//criando uma enxada de diamante
 const enxada3 = new Enxada(10, "diamante", false)
 enxada3.criarEnxada()
 enxada3.minerar()
+
+
+//Criando uma Espada de madeira
+const espada1 = new Espada(2, "Madeira")
+espada1.criarEspada()
+
+//Criando uma Espada de ferro
+const espada2 = new Espada(5, "ferro")
+espada2.criarEspada()
+espada2.minerar()
+
+//Criando uma Espada de Diamante
+const espada3 = new Espada(10, "Diamante")
+espada3.criarEspada()
+espada3.minerar()
+
+
+//Criando uma vara de pesca de madeira
+const pesca1 = new VaraDePesca(2, "Madeira")
+pesca1.criarvara()
+
+//criando uma vara de pesca de ferro
+const pesca2 = new VaraDePesca(5, "Ferro", true)
+pesca2.criarvara()
+pesca2.pescar()
